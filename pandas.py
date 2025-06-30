06fdfe8b79c1daa703ee881d01621aecfe35673f
komerishetty revanth <revanthgputha@gmail.com>
2025-06-30 12:35:08 +0530

    code

--- pandas.py
+++ pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv('sales_data.csv')
+df = pd.read_csv('sales.csv')
 print(df.head())
 grouped = df.groupby('Product')['Sales'].sum()
 grouped.plot(kind='bar', title='Total Sales by Product', xlabel='Product', ylabel='Total Sales', figsize=(10, 6), color='skyblue')
